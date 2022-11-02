67ee4dd0d9f701b364f2c123eff9110d0712a0d2
gmmartins <gmmartins06@gmail.com>
2022-11-02 01:13:19 -0300

    Dia primeiro concluid

---
+++ web/pages/index.tsx
@@ -0,0 +1,21 @@
+interface HomeProps {
+  count: number
+}
+
+export default function Home({ count }: HomeProps) {
+
+  return (
+    <h1>Total de bolões: {count}</h1>
+  )
+}
+
+export const getServerSideProps = async () => {
+  const response = await fetch('http://localhost:3333/pools/count')
+  const { count } = await response.json()
+
+  return {
+    props: {
+      count
+    }
+  }
+}
